fcc6809bbba29de80d5d29da91610ad4f93db224
A2gamer2345V <A2gamer2345v@gmail.com>
2026-03-27 19:35:55 +0530

    Delete scr/main/java/com/A2plugin/lsreborn directory

--- scr/main/java/com/A2plugin/lsreborn/util/worldguard/HeartLossFlag.java
+++
@@ -1,9 +0,0 @@
-package com.A2plugin.lsreborn.util.worldguard;
-
-import com.sk89q.worldguard.protection.flags.StateFlag;
-
-public final class HeartLossFlag extends StateFlag {
-    public HeartLossFlag() {
-        super("heartloss", true);
-    }
-}
